54bcd0eb99348522b4efea9dcdfca5d09fe0392a
wddfrt <evandbai@gmail.com>
2024-01-12 10:27:22 -0500

    commit

---
+++ draw.py
@@ -0,0 +1,11 @@
+# File: draw.py
+# Name: Evan Bai, Sami Ahmed
+# Date: Jan 08th 2024
+# Class: ICS4UI
+# Asteroid Game culminating assignment
+
+def DrawImage():
+    pass
+
+def DrawShape():
+    pass
