aba120213778a531dea1ef21e51944df66bce053
wddfrt <evandbai@gmail.com>
2024-01-21 23:57:27 -0500

    commit

--- drawings.py
+++ drawings.py
@@ -6,11 +6,25 @@
 
 import pygame
 
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
+
 def bruh():
     print("ok")
 
+def LoadImage(path):
+    return pygame.image.load(path)
+
 def Resize(img, x, y):
-    pygame.transform.scale(img, (x, y))
+    return pygame.transform.scale(img, (x, y))
 
 
 def DrawImage(screen, image, x, y):
@@ -19,3 +33,13 @@ def DrawImage(screen, image, x, y):
 
 def DrawShape():
     pass
+
+# Images 
+
+
+# Ship for Player 1
+shipimg1 = LoadImage("mcqueen.png")
+## Ship for Player 2
+shipimg2 = LoadImage("man.jpg")
+# Background image
+bgimg1 = pygame.image.load('bg.jpg')
--- main.py
+++ main.py
@@ -11,37 +11,22 @@ import math
 import random
 
 # import module
-import bruh
 import drawings
-
-bruh.asdf()
 
 ## Constant Variables
 
 spriteclass = pygame.sprite.Sprite
 Display = pygame.display
-PLAYERSIZE = 50
+PLAYERSIZE = 30
 FPS = 30
 
 # Width & Height
 WIDTH = 1200
-HEIGHT = 800
+HEIGHT = 700
 
 # Colors
 BLACK = (0, 0, 0)
 WHITE = (255,255,255)
-
-# Images 
-
-# Ship for Player 1
-shipimg1 = pygame.image.load('mcqueen.png')
-## Ship for Player 2
-shipimg2 = pygame.image.load('download.png')
-# Asteroid Image
-asteroidimg = pygame.image.load('woman.png')
-# Background image
-bgimg1 = pygame.image.load('bg.jpg')
-
 
 # Lists
 
@@ -55,97 +40,125 @@ pygame.display.set_caption("Asteroid") # Caption
 # Classes
 # Player Class
 class Players(spriteclass):
+  PlayerGroup = pygame.sprite.Group()
 
   # Initialization
   def __init__(self, x, y, img):
-    
-    # Initial (x, y) coordinates
-    self.x = x
-    self.y = y
+    spriteclass.__init__(self)
+    self.PlayerGroup.add(self)
 
     # Image & Initial Angle (forward)
-    self.img = img
-    self.rect = self.img.get_rect()
+    self.rootimg = drawings.Resize(img, PLAYERSIZE, PLAYERSIZE)
+    self.image = self.rootimg
+    self.rect = self.rootimg.get_rect()
     self.angle = 0
+
+    # Initial (x, y) coordinates
+    self.rect.topleft = (x, y)
 
     # Size and initial speed
     self.size = PLAYERSIZE
-    self.speed = 50
+    self.speed = 20
 
 
   # Function to move left
   def moveLeft(self):
-    self.x -= self.speed
-    self.angle = 90
-    self.img = pygame.transform.rotate(self.img, self.angle)
+    self.angle += math.pi / 20
+    self.image = drawings.rot_center(self.rootimg, math.degrees(self.angle))
 
 
   # Function to move right
   def moveRight(self):
-    self.x += self.speed
-    self.angle = 270
-
+    self.angle -= math.pi / 20
+    self.image = drawings.rot_center(self.rootimg, math.degrees(self.angle))
+    
 
   # Function to move forward
   def moveForward(self):
-    self.y -= self.speed
-    self.angle = 0
-
+    self.rect.move_ip(-(self.speed * math.sin(self.angle)),-(self.speed * math.cos(self.angle)))
+    self.image = drawings.rot_center(self.rootimg, math.degrees(self.angle))
 
   # Function to move backward
   def moveBack(self):
-    self.y += self.speed
-    self.angle = 180
-  
+    self.rect.move_ip(-(self.speed * math.sin(self.angle)),(self.speed * math.cos(self.angle)))
+    self.image = drawings.rot_center(self.rootimg, math.degrees(self.angle))
+
 
 # Asteroid Class
 class Asteroids(spriteclass):
+  sourceimage = drawings.LoadImage("woman.png")
 
-  # Table for all asteroids created
-  AsteroidTable = []
+  AsteroidGroup = pygame.sprite.Group()
   lasttick = pygame.time.get_ticks()
   
   # Initialization
   def __init__(self):
+    spriteclass.__init__(self)
+
+    self.AsteroidGroup.add(self)
 
     # All attributes of asteroids created are randomized
-    self.speed = random.randint(1,10)
-    self.size = random.randint(1,10)
-    x = random.choice([True, False])
+    self.size = random.randint(20,50)
+    self.speed = random.randint(1,2)
+    self.angle = random.randint(-2,2)
+    self.image = drawings.Resize(self.sourceimage, self.size, self.size)
+    self.rect = self.image.get_rect()
 
+    x = random.choice([True, False])
     # Chooses if the asteroid will be placed along the x axis or the y axis randomly
     if x:
       self.x = random.randint(0, WIDTH - self.size)
-      self.y = random.choice([self.size, HEIGHT])
+      self.y = random.choice([0, HEIGHT - self.size])
+      self.speedx = self.angle
+
+      if self.y == 0:
+        self.speedy = self.speed
+      else:
+        self.speedy = -self.speed
+
     else:
+      self.speedy = self.angle
       self.y = random.randint(self.size, HEIGHT)
       self.x = random.choice([0, WIDTH - self.size])
 
-  def draw(self):
-    drawings.DrawImage(Display, asteroidimg, self.x, self.y)
+      if self.x == 0:
+        self.speedx = self.speed
+      else:
+        self.speedx = -self.speed
+
+
+    self.rect.topleft = (self.x,self.y)
+
+
+  def update(self):
+    self.rect.move_ip(self.speedx,self.speedy)
 
 
 # Bullets Class
 class Bullets(spriteclass):
-  # List of all bullets on screen
-  BulletTable = []
 
   # Initialization
   def __init__(self):
+    spriteclass.__init__()
     pass
 
 
 # Powerups Class
 class Powerups(spriteclass):
-  PowerupTable = []
 
   # Initialization
   def __init__(self):
+    spriteclass.__init__()
     pass
     
 
-# Creates image size constants
-drawings.Resize(bgimg1, WIDTH, HEIGHT)
+background = drawings.Resize(drawings.bgimg1, WIDTH, HEIGHT)
+
+
+player1img = pygame.transform.rotate(drawings.shipimg1, 180)
+
+player1 = Players(200, 200, player1img) 
+player2 = Players(WIDTH - 200, HEIGHT - 200, drawings.shipimg2)
 
 # Updates Display
 while True:
@@ -154,23 +167,55 @@ while True:
     if event.type == pygame.QUIT:
           pygame.quit()
 
-  
-  drawings.DrawImage(screen, "BG1", 0,0)
+  drawings.DrawImage(screen, background , 0,0)
 
 
   # Player 1 and 2 movement
   key = pygame.key.get_pressed()
 
-  if key[pygame.K_w]:
-    pass
+  if key[pygame.K_w] and player1.rect.y - player1.speed > 0:
+    player1.moveForward()
+
+  if key[pygame.K_a] and player1.rect.x - player1.speed > 0:
+    player1.moveLeft()
+
+  if key[pygame.K_s] and player1.rect.y + player1.speed < HEIGHT - PLAYERSIZE:
+    player1.moveBack()
+  
+  # THESE MAKE NO SENSE SAMI
+
+  if key[pygame.K_d] and player1.rect.x + player1.speed < WIDTH - PLAYERSIZE:
+    player1.moveRight()
+
+  if key[pygame.K_UP] and player2.y - player2.speed > 0:
+    player2.moveForward()
+
+  if key[pygame.K_DOWN]:
+    player2.moveBack()
+
+  if key[pygame.K_LEFT]:
+    player2.moveLeft()
+
+  if key[pygame.K_RIGHT]:
+    player2.moveRight()
+
+  # Draw Players 1 and 2
+  
+  Players.PlayerGroup.draw(screen)
+
 
 
   # Asteroid Spawn
+
+  Asteroids.AsteroidGroup.update()
+  Asteroids.AsteroidGroup.draw(screen)
+  
   
   # spawns an asteroid every 0.5 seconds
   if pygame.time.get_ticks() - Asteroids.lasttick > 500:
-    t = Asteroids()
-    t.draw()
+    Asteroids.lasttick = pygame.time.get_ticks()
+    print("cock")
+    Asteroids()
           
   # Powerup spawn
   
@@ -178,4 +223,3 @@ while True:
   # Updates Screen
   pygame.time.Clock().tick(FPS)
   Display.update()
-  print("penispenis")
